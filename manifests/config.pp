856159d8469be952f5bdb06a1ee10b26eaa808bd
Bryan Belanger <bryan@autostructure.io>
2017-12-08 15:16:41 -0500

    Put back ip6tables check.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,10 @@
 # Install Kubernetes required packages
 class kubernetes::config {
-  # sysctl { 'net.bridge.bridge-nf-call-ip6tables':
-  #   ensure => present,
-  #   value  => '1',
-  # }
-#
+  sysctl { 'net.bridge.bridge-nf-call-ip6tables':
+    ensure => present,
+    value  => '1',
+  }
+
   sysctl { 'net.bridge.bridge-nf-call-iptables':
     ensure => present,
     value  => '1',
